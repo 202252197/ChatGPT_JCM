96e8088a37691d94a8868a614c5dda808eb20e75
fiyen <623320480@qq.com>
2023-03-18 17:45:22 +0800

    add model descriptions,set gpt-3.5-turbo to top

--- src/api/getData.js
+++ src/api/getData.js
@@ -1,6 +1,61 @@
 import base from './index'
 let axios = base.axios
 let baseUrl = base.baseUrl
+
+// 根据name查找元素的索引
+function findIndexByName(arr, name) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].name === name || arr[i] === name) {
+      return i;
+    }
+  }
+  return -1; // 没有找到对应的元素
+}
+
+const desp_model = {"gpt-3.5-turbo": "chatgpt v3.5 所基于的模型",
+                    "ada": "自然语言模型，OpenAI提供的最快，最便宜的模型，但性能也最差，含有ada字眼的模型都是基于ada训练而来",
+                    "babbage": "自然语言模型，性能比ada强，价格比ada贵，规模比ada大，含有babbage字眼的模型都是基于babbage训练而来",
+                    "curie": "自然语言模型，性能优于ada，babbage，价钱也更贵，规模更大，含有curie字眼的模型都是基于curie训练而来",
+                    "davinci": "自然语言模型，在ada，babbage，curie和davinci中性能最优，规模最大，速度最慢，价钱最贵，含有davinci字眼的模型都是基于davinci训练而来，目前chatgpt基于davinci微调而来",
+                    "whisper-1": "强大的语音转换文本的模型"
+                  }
+const other_desps = {"code": "的AI代码处理模型",
+                  "similarity": "的AI文本相似度计算模型",
+                 "document": "的大文档处理模型",
+               "text": "的文本处理模型",
+             "instruct": "的人工指令微调模型",
+           "if": "一个分支"}
+const desp_keys = Object.keys(desp_model)
+const other_desp_keys = Object.keys(other_desps)
+
+function produceModelDesc(model) {
+  const idx = findIndexByName(desp_keys, model)
+  if (idx !== -1) {
+    return desp_model[model]
+  }
+  else {
+    let desc = '';
+    for (let i=0; i<desp_keys.length; i++) {
+      const key = desp_keys[i];
+      if (model.includes(key)) {
+        desc += `基于语言模型${key}`;
+        break;
+      }
+    }
+    for (let i=0; i<other_desp_keys.length; i++) {
+      const key = other_desp_keys[i];
+      if (model.includes(key)) {
+        desc += other_desps[key];
+        break;
+      }
+    }
+    if (desc == "") {
+      desc = model + "模型"
+    }
+    return desc
+  }
+}
+
 const statements = ["十五日得半卷诗稿，遇墨迹化魂水中。",
                     "—朝有悟，臻至化境。",
                   "神游千山外，化墨入深潭。",
@@ -33,7 +88,7 @@ export const getModels = token => {
       let modelObj={
         img: "",
         name: model,
-        detail: statements[Math.floor(Math.random() * statements.length)],
+        detail: produceModelDesc(model),
         lastMsg: model+"模型",
         id: model,
         headImg: require("@/assets/img/ai.png"),
@@ -41,6 +96,12 @@ export const getModels = token => {
       }
       modelsObj.push(modelObj)
     });
+    // 将gpt-3.5-turbo置顶
+    const idx = findIndexByName(modelsObj, "gpt-3.5-turbo")
+    if (idx !== -1) {
+      const element = modelsObj.splice(idx, 1)[0]; // 将idx元素删除
+      modelsObj.unshift(element); // 将idx出的元素至于列表头
+    }
     return modelsObj;
   })
 }
